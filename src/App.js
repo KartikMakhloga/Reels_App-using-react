8fce30ceb606d927c9c92ca63b3ad0fcbada3720
kartik <kartikmakhloga20@gmail.com>
2023-02-09 16:42:14 +0530

    learning orderBy and limit function of firestore

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { firestore } from "./firebase";
 function App() {
   useEffect(() => {
     let f = async () => {
-      let querySnapshot = await firestore.collection("posts").get();
+      let querySnapshot = await firestore.collection("posts").limit(3).orderBy("index","asc").get();
       querySnapshot.forEach((doc)=>{
-        console.log(doc.id);
+        console.log(doc.data());
       })
     };
+    f();
   }, []);
 
   return <h1>hello</h1>;
 }
 
-export default App;``
+export default App;
