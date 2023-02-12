7c7fea9d122ad6a3c2f25209d245eebfb9e9a406
kartik <kartikmakhloga20@gmail.com>
2023-02-12 13:17:19 +0530

    added like feature

--- src/VideoCard.jsx
+++ src/VideoCard.jsx
@@ -1,8 +1,19 @@
 function VideoCard() {
   return (
     <div className="video-card">
-      <span class="material-icons-outlined like">favorite_border</span>
-      <span class="material-icons-outlined comment">comment</span>
+      <span
+        className="material-icons-outlined like"
+        onClick={(el) => {
+          if (el.currentTarget.innerText === "favorite_border") {
+            el.currentTarget.innerText = "favorite";
+          } else {
+            el.currentTarget.innerText = "favorite_border";
+          }
+        }}
+      >
+        favorite_border
+      </span>
+      <span className="material-icons-outlined comment">comment</span>
       <p className="username"></p>
       <p className="song"></p>
     </div>
